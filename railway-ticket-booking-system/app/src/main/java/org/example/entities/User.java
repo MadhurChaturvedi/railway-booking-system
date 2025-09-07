5a8e63194f94f6b7fe3f0f5a25a2dbfdab9e54ed
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2025-09-07 18:53:03 +0530

    User Entities created

---
+++ railway-ticket-booking-system/app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,12 @@
+package org.example.entities;
+
+//Here we create the User Entities for referrence which we are using name password hashPassword ticketBooked
+
+public class User{
+
+    private  String name;
+    private  String password;
+    private  String hashPassword;
+    private  List<Ticket> ticketBooked;
+
+}
